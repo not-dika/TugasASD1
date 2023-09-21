f92bbe43f94b4cffd1bd73c4bb36138c959bdee7
not-dika <145648587+not-dika@users.noreply.github.com>
2023-09-21 15:53:36 +0700

    Psudocode

---
+++ main.java
@@ -0,0 +1,13 @@
+package main;
+
+public static void main(String args[]) {
+    int alas = 15;
+    int tinggi = 23;
+    int luas = alas * tinggi;
+    if (luas%2 == 0) {
+        System.out.println("Genap")
+    } else {
+        System.out.println("Ganjil")
+    }
+
+}
