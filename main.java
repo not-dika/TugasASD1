a3ec5e4ad655d6d85e51c44bc8ca3edcd871c1d6
not-dika <yukimak420@gmail.com>
2023-09-21 22:15:44 +0700

    Fix Pseudocode into Real Code

--- main.java
+++ main.java
@@ -1,13 +1,14 @@
 package main;
 
-public static void main(String args[]) {
-    int alas = 15;
-    int tinggi = 23;
-    int luas = alas * tinggi;
-    if (luas%2 == 0) {
-        System.out.println("Genap")
-    } else {
-        System.out.println("Ganjil")
+public class main {
+    public static void main(String[] args) {
+        int alas = 15;
+        int tinggi = 23;
+        int luas = alas * tinggi / 2;
+        if (luas % 2 == 0) {
+            System.out.println("Luas: " + luas + " Genap");
+        } else {
+            System.out.println("Luas: " + luas + " Ganjil");
+        }
     }
-
 }
