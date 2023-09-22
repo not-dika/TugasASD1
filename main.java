9d78cbc023835bc79b451705aac10d0cc4c5b1a1
not-dika <145648587+not-dika@users.noreply.github.com>
2023-09-22 17:12:25 +0700

    Fix issue on result

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class main {
     public static void main(String[] args) {
         int alas = 15;
         int tinggi = 23;
-        int luas = alas * tinggi / 2;
+        double luas = 0.5 * alas * tinggi;
         if (luas % 2 == 0) {
             System.out.println("Luas: " + luas + " Genap");
         } else {
